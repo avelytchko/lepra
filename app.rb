48d1704deba0b94e7f32d1df59dca453dea7866b
Rand <avelytchko@gmail.com>
2016-01-29 02:42:19 +0200

    add index view for posts

--- app.rb
+++ app.rb
@@ -1,14 +1,28 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+require 'tilt/erb'
+
+def init_db
+  @db = SQLite3::Database.new 'lepra.db'
+  @db.results_as_hash = true
+end
 
 configure do
   enable :sessions
+  init_db
+  @db.execute 'CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, created_date DATE, content TEXT);'
 end
 
 helpers do
   def username
     session[:identity] ? session[:identity] : 'Hello stranger'
   end
+end
+
+before '/new' do
+  init_db
 end
 
 before '/secure/*' do
@@ -40,4 +54,22 @@ end
 
 get '/secure/place' do
   erb 'This is a secret place that only <%=session[:identity]%> has access to!'
+end
+
+get '/new' do
+  erb :newpost
+end
+
+post '/new' do
+  @content = params[:content]
+
+  if @content.length <= 0
+    @error = "Введите текст"
+  else
+    @message = "Новый пост создан!"
+  end
+
+  @db.execute 'insert into posts (content, created_date) values (?, datetime())', [@content]
+
+  erb :newpost
 end
